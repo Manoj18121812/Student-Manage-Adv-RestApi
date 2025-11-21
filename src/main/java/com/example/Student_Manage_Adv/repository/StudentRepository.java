61a97e1f2206b6a4b8765f3470d0434382538d1d
Manoj18121812 <gavhaneam@gmail.com>
2025-11-21 23:19:17 +0530

    Repo created

--- src/main/java/com/example/Student_Manage_Adv/repository/StudentRepository.java
+++ src/main/java/com/example/Student_Manage_Adv/repository/StudentRepository.java
@@ -1,5 +1,14 @@
 package com.example.Student_Manage_Adv.repository;
 
-public interface StudentRepository {
+import com.example.Student_Manage_Adv.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+    List<Student> findByCity(String city);
+    List<Student> findByNameContaining(String namePart);
+    List<Student> findByMarksGreaterThan(double marks);
+    List<Student> findByMarksLessThan(double marks);
 }
